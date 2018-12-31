959751317f78ed73340c4823c749bad216b50b39
Josh Griffith <josh@starshipconsulting.com>
2018-12-31 13:09:30 -0500

    commit

--- Providers/Cosmos/AzureDocumentDbProvider.cs
+++ Providers/Cosmos/AzureDocumentDbProvider.cs
@@ -7,11 +7,13 @@ using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Newtonsoft.Json;
 using Starship.Azure.Json;
+using Starship.Core.Extensions;
 
 namespace Starship.Azure.Providers.Cosmos {
     public class AzureDocumentDbProvider {
         
         public AzureDocumentDbProvider(string uri, string authenticationKey, string databaseName) {
+            ForceCamelcase = true;
             Client = new DocumentClient(new Uri(uri), authenticationKey);
             DatabaseName = databaseName;
             DatabaseUri = UriFactory.CreateDatabaseUri(DatabaseName);
@@ -20,15 +22,15 @@ namespace Starship.Azure.Providers.Cosmos {
             };
         }
 
-        public async Task<string> Save(string collectionName, object entity) {
+        public async Task<Document> Save(string collectionName, object entity) {
             var collection = await GetCollection(collectionName);
             var result = await Client.UpsertDocumentAsync(collection.GetDocumentUri(), entity, new RequestOptions { JsonSerializerSettings = Settings });
-            return result.Resource.Id;
+            return result.Resource;
         }
 
         public IEnumerable<string> GetCollections() {
             var collections = Client.CreateDocumentCollectionQuery(DatabaseUri);
-            return collections.Select(each => each.Id).ToList();
+            return collections.Select(each => each.Id).ToList().OrderBy(each => each).ToList();
         }
 
         public async Task<IEnumerable<T>> Get<T>(string collectionName) {
@@ -36,10 +38,31 @@ namespace Starship.Azure.Providers.Cosmos {
             return Client.CreateDocumentQuery<T>(collection.GetCollectionUri());
         }
 
+        public async Task<IEnumerable<dynamic>> Get(string collectionName, SqlQuerySpec sqlQuery) {
+            var collection = await GetCollection(collectionName);
+            IQueryable<dynamic> query;
+
+            if(sqlQuery != null) {
+                query = Client.CreateDocumentQuery(collection.GetCollectionUri(), sqlQuery);
+            }
+            else {
+                query = Client.CreateDocumentQuery(collection.GetCollectionUri());
+            }
+            
+            return query;
+        }
+
         public async Task<IEnumerable<Document>> Get(string collectionName, int take = 0, SqlQuerySpec sqlQuery = null) {
             var collection = await GetCollection(collectionName);
-            var query = Client.CreateDocumentQuery<Document>(collection.GetCollectionUri(), sqlQuery);
+            IQueryable<Document> query;
 
+            if(sqlQuery != null) {
+                query = Client.CreateDocumentQuery<Document>(collection.GetCollectionUri(), sqlQuery);
+            }
+            else {
+                query = Client.CreateDocumentQuery<Document>(collection.GetCollectionUri());
+            }
+            
             if(take > 0) {
                 return query.Take(take);
             }
@@ -68,11 +91,13 @@ namespace Starship.Azure.Providers.Cosmos {
         }
 
         public async Task<ResourceResponse<DocumentCollection>> CreateCollection(string collectionName) {
-            return await Client.CreateDocumentCollectionIfNotExistsAsync(DatabaseUri, new DocumentCollection { Id =  collectionName });
+            collectionName = ResolveCollectionName(collectionName);
+            return await Client.CreateDocumentCollectionIfNotExistsAsync(DatabaseUri, new DocumentCollection { Id =  collectionName }, new RequestOptions{ OfferThroughput = MinimumThroughput });
         }
 
         private async Task<AzureDocumentCollection> GetCollection(string collectionName) {
-            
+            collectionName = ResolveCollectionName(collectionName);
+
             await Semaphore.WaitAsync();
 
             try {
@@ -88,6 +113,15 @@ namespace Starship.Azure.Providers.Cosmos {
             }
         }
 
+        private string ResolveCollectionName(string name) {
+            if(ForceCamelcase) {
+                return name.CamelCase();
+            }
+            return name;
+        }
+
+        public bool ForceCamelcase { get; set; }
+        
         private string DatabaseName { get; set; }
 
         private Uri DatabaseUri { get; set; }
@@ -99,5 +133,7 @@ namespace Starship.Azure.Providers.Cosmos {
         private static readonly Dictionary<string, AzureDocumentCollection> Collections = new Dictionary<string, AzureDocumentCollection>();
 
         private static readonly SemaphoreSlim Semaphore = new SemaphoreSlim(1, 1);
+
+        private const int MinimumThroughput = 400;
     }
 }
